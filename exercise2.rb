e75bb7acd814d3327caaf7e583516cd781632063
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-01-12 14:40:01 -0500

    complete exercixe 2 from Article

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+
+
+puts "A good tip for your meal is: #{55.00*0.18}"
+
+puts "I am " + 30.to_s  + " years old."
+
+puts "The answer is: #{45628 * 7839}."
+
+question_four = (10 < 20 && 30 < 20) || !(10 == 11)
+
+puts "The anser for is True as validated by the code" + question_four.to_s
