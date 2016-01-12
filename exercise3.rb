9c9fe819baa2e594961ac1e91e3dcae366373567
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-01-12 15:00:04 -0500

    Update to calculate birth year.

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,12 @@
 puts "What's is your name?"
-name = gets.strip
-# name = gets.chomp
+# name = gets.strip
+name = gets.chomp
 
 puts "Hi #{name}"
+
+puts "How old are you, #{name}?"
+age = gets.chomp.to_i
+
+birth_year = (2015 - age).to_s
+
+puts "#{name} that would mean you were born in #{birth_year}."
